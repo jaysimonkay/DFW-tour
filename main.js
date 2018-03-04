543d07ded53e11cc5171d2000313725cf7d8c572
Jude Kuti <judekuti@gmail.com>
2018-03-04 15:41:27 -0600

    Include map div, linked html to main.js and wrote initial script for DFW map

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-main.js
+var map;
+      function initMap() {
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat:  ‎32.768799, lng: -97.309341},
+          zoom: 13
+        });
+      }
